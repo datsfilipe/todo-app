d21d08df76c6ae684089f0a8aa4808307d395c76
datsfilipe <datsfilipe.foss@proton.me>
2023-07-14 16:13:09 -0300

    feat: add clean option in webpack build output

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    clean: true
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
